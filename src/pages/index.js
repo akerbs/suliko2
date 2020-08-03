1f9c7326ff91cbec8d83598fd2256cfd1e27c353
Anatol Kerbs <anker2702@gmail.com>
2020-08-03 10:26:18 +0200

    030820_5

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,6 +68,7 @@ const useStyles = makeStyles(theme => ({
     top: 0,
     left: 0,
     display: "flex",
+    width: " 100%",
     height: "100%",
     // alignItems: "center",
     color: "white",
