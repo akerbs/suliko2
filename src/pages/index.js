860ee69705601757759e2a19ccc20f6ae8c0d6a3
Anatol Kerbs <anker2702@gmail.com>
2020-08-03 11:27:53 +0200

    030820_13

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ const useStyles = makeStyles(theme => ({
   //   },
   // },
   second: {
-    display: "flex",
+    // display: "flex",
     width: " 100%",
     height: "100vh",
     backgroundSize: "cover",
