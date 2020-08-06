dc7d6add01f72f22a92b6fe45613d1b41d6eddb4
Anatol Kerbs <anker2702@gmail.com>
2020-08-06 14:52:48 +0200

    new11

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ import CloseIcon from "@material-ui/icons/Close"
 import bgPatternImg from "../images/bgPatternImg.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons"
-import LangSwAkk from "./langSwAkk"
+// import LangSwAkk from "./langSwAkk"
 import Button from "@material-ui/core/Button"
 import { bgImgPosBb, bgImgPosBbV } from "../pages/index"
 
@@ -260,9 +260,9 @@ function Header(props) {
         })}
       >
         <Toolbar className={classes.toolbar}>
-          <Hidden smUp>
+          {/* <Hidden smUp>
             <LangSwAkk />
-          </Hidden>
+          </Hidden> */}
 
           <Hidden xsDown>
             <Link to="/">
@@ -280,9 +280,9 @@ function Header(props) {
           </Link>
 
           <div style={{ display: "flex", justifyContent: "column" }}>
-            <Hidden xsDown>
+            {/* <Hidden xsDown>
               <LangSwAkk />
-            </Hidden>
+            </Hidden> */}
 
             <IconButton
               size="small"
